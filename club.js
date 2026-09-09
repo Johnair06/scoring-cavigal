6ff8402c12be64bb54ad295148c3ecce8acb79d9
Johnair06 <93306577+Johnair06@users.noreply.github.com>
2026-09-09 21:40:43 +0200

    Add files via upload

---
+++ club.js
@@ -0,0 +1,14 @@
+const CLUBS = [
+  { name: "BHB BILLÈRE", logo: "clubs/logo-billiere-hbp.png" },
+  { name: "BRUGES 33", logo: "clubs/logo-bruges33.png" },
+  { name: "CAVIGAL NICE", logo: "clubs/logo-cavigal-nice-hb.png" },
+  { name: "GRAND LIBOURNE", logo: "clubs/logo-ent-grand-libournais-handball.png" },
+  { name: "FENIX TOULOUSE", logo: "clubs/logo-Fenix_Toulouse_Handball_2.png" },
+  { name: "GFCA AJACCIO", logo: "clubs/logo-gfca-hb.png" },
+  { name: "MONTPELLIER HB", logo: "clubs/logo-montpellier-hb.png" },
+  { name: "PAUC AIX", logo: "clubs/logo-pays-aix-universite-hb.png" },
+  { name: "ROC AVEYRON", logo: "clubs/logo-roc-aveyron-hb.png" },
+  { name: "ST-RAPHAËL VHB", logo: "clubs/logo-SaintRaphaelVarHandball-2024.png" },
+  { name: "US CRAUROISE", logo: "clubs/logo-us-crauroise.png" },
+  { name: "USAM NÎMES", logo: "clubs/nimes_logo_2023-2024.png" }
+];
